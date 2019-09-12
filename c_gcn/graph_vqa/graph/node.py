7cc20bcc960c23003b836c9456149fa496e6b968
chengyang317 <chengyang317@gmail.com>
2019-09-12 21:59:24 +0800

    sync

--- c_gcn/graph_vqa/graph/node.py
+++ c_gcn/graph_vqa/graph/node.py
@@ -7,6 +7,8 @@ __all__ = ['Node']
 
 
 class Node(object):
+    caches = {}
+
     def __init__(self, graph, node_feats, node_boxes=None, node_weights=None, node_valid_nums=None):
         assert node_feats.dim() == 3
         self.graph = graph
@@ -17,7 +19,7 @@ class Node(object):
         self.feat_layers = collections.defaultdict(None)
         self.logit_layers = {}
         self.mask = None
-        self.old2new_map = torch
+        self.old2new_map = self.old2new_cache.cuda(node_feats.device)
         if self.valid_nums is not None:
             self.mask = torch.arange(self.node_num).expand(self.batch_num, -1).cuda(self.device) < self.valid_nums[:, None]
             self.feats = node_feats[self.mask]
@@ -25,6 +27,27 @@ class Node(object):
         else:
             self.mask = torch.ones(self.batch_num, self.node_num).cuda(self.device).bool()
             self.feats = node_feats.view(-1, self.feat_num)
+
+    @property
+    def old2new_cache(self):
+        key = f'old2new_{self.batch_num}_{self.node_num}'
+        if key not in self.caches:
+            self.caches[key] = torch.full((self.batch_num*self.node_num, ), fill_value=-1).long()
+        return self.caches[key]
+
+    @property
+    def mask_cache(self):
+        key = f'mask_{self.batch_num}_{self.node_num}'
+        if key not in self.caches:
+            self.caches[key] = torch.ones(self.batch_num, self.node_num).bool()
+        return self.caches[key]
+
+    @property
+    def node_num_cache(self):
+        key = f'node_num_{self.batch_num}_{self.node_num}'
+        if key not in self.caches:
+            self.caches[key] = torch.arange(self.node_num).expand(self.batch_num, -1).contiguous()
+        return self.caches[key]
 
     @property
     def edge(self):
